1f573418fe3c882bdb78ab702f29554d1f50b18c
John Bars <john.barsq@gmail.com>
2024-02-11 18:28:50 +0800

    move the portfolio projects to lib -shorten the code by moving the info to data.js and  map it to be used for the ProjectInfo

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,6 +3,7 @@
 import { useSectionInView } from "@/lib/hooks";
 import { useState } from "react";
 import { ProjectInfo } from ".";
+import { portfolioProjects } from "@/lib/data";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects");
@@ -19,38 +20,16 @@ export default function Projects() {
     <section id="projects" ref={ref} className="section">
       <h2 className="title">My Projects</h2>
       <div className="flex flex-col gap-5">
-        <ProjectInfo
-          title="DevFlow"
-          info={
-            <>
-              Devflow is a web app built using Next.js and TypeScript. It&apos;s
-              similar to Stack Overflow where users can ask questions to the
-              community. Additionally, it utilizes OpenAI&apos;s API to generate
-              AI answers and provides users with the ability to find available
-              jobs.
-            </>
-          }
-          isVisible={visibleInfo === "DevFlow"}
-          onClick={toggleInfo}
-          images={{
-            web: [
-              "/images/devflow-web-dark.png",
-              "/images/devflow-web-light.png",
-            ],
-            mobile: [
-              "/images/devflow-tablet-dark.png",
-              "/images/devflow-tablet-light.png",
-              "/images/devflow-mobile-dark.png",
-              "/images/devflow-mobile-light.png",
-            ],
-          }}
-        />
-        <ProjectInfo
-          title="Portfolio Website"
-          info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-          isVisible={visibleInfo === "Portfolio Website"}
-          onClick={toggleInfo}
-        />
+        {portfolioProjects.map((project) => (
+          <ProjectInfo
+            key={project.title}
+            title={project.title}
+            info={project.info}
+            isVisible={visibleInfo === project.title}
+            onClick={toggleInfo}
+            images={project.images}
+          />
+        ))}
       </div>
     </section>
   );
