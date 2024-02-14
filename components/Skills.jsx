695e710aef2868cf369f3f4698b07bb6d2249a82
John Bars <john.barsq@gmail.com>
2024-02-14 18:39:46 +0800

    Skills component

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { skills } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Skills() {
@@ -7,8 +8,17 @@ export default function Skills() {
 
   return (
     <section ref={ref} id="skills" className="section">
-      <h2 className="title">Skills</h2>
-      <div>Skills</div>
+      <h2 className="title">Skills</h2>{" "}
+      <ul className="flex max-w-sm flex-wrap gap-4 lg:max-w-md">
+        {skills.map((skill, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-center rounded-full border border-neutral-400 p-5 text-sm text-neutral-400 md:p-6 md:text-base lg:p-8"
+          >
+            {skill}
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
