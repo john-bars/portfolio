f4a0bb5eb6a285b22c1ed9ff22704a7bf080fbf9
John Bars <john.barsq@gmail.com>
2024-02-15 18:07:44 +0800

    pass styled info component  form the lib

--- components/ProjectInfo.jsx
+++ components/ProjectInfo.jsx
@@ -19,21 +19,25 @@ const ProjectInfo = ({
     <div className="flex flex-col items-start gap-4">
       <button
         onClick={() => onClick(title)}
-        className={`${isVisible ? "border-b-blue-500/80 text-blue-500/80" : "border-b-neutral-700 text-neutral-400"} flex w-full cursor-pointer justify-between border-b-2  pb-2 text-4xl font-medium md:text-5xl`}
+        className={`${isVisible ? "border-transparent text-blue-500/80" : "border-b-neutral-700 text-neutral-400"} flex w-full cursor-pointer justify-between border-b-2  pb-2 text-4xl font-medium md:text-5xl`}
       >
         <span className="truncate"> {title}</span>
         {isVisible ? <RiSubtractFill className="" /> : <MdAdd />}
       </button>
       {isVisible && (
         <>
-          <p className="text-neutral-400">{info}</p>
+          <p>{info}</p>
           <div className="flex gap-5">
-            <Link href={code} target="_blank">
-              <button className="btn">Code</button>
-            </Link>
-            <Link href={site} target="_blank">
-              <button className="btn">Site</button>
-            </Link>
+            {code && (
+              <Link href={code} target="_blank">
+                <button className="btn">Code</button>
+              </Link>
+            )}
+            {site && (
+              <Link href={site} target="_blank">
+                <button className="btn">Site</button>
+              </Link>
+            )}
           </div>
           {images && (
             <div className="custom-scrollbar flex h-[350px] w-full gap-5 overflow-x-auto">
@@ -65,7 +69,7 @@ const ProjectInfo = ({
             {tech.map((item, index) => (
               <li
                 key={index}
-                className="rounded-xl border border-neutral-700 px-3 py-2 text-neutral-500"
+                className="rounded-xl border border-neutral-700 px-3 py-2 font-medium text-neutral-500"
               >
                 {item}
               </li>
