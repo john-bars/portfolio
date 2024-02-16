080904c43dcdf5d95bf48db0588e40f548f14e1f
John Bars <john.barsq@gmail.com>
2024-02-16 19:46:35 +0800

    test: deploy

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "John Bars Quipia",
   description:
-    "Hi! I'm John Bars, a frontend developer specializing in React and Next.js.",
+    "Hi! I'm John Bars, a frontend developer specializing in React and Next.js",
   image:
     "https://john-bars.vercel.app/_next/image?url=%2Fimages%2Fportfolio-mobile.png&w=384&q=75",
 };
