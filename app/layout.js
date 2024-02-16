ae9f81e3cffbbadd32e768429ed445ca2cbfbbaa
John Bars <john.barsq@gmail.com>
2024-02-16 19:40:50 +0800

    test for image in metadata

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,15 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  openGraph: {
-    title: "John Bars Quipia",
-    description:
-      "Hi! I'm John Bars, a frontend developer specializing in React and Next.js.",
-    Images: {
-      url: "https://john-bars.vercel.app/_next/image?url=%2Fimages%2Fjohn-bars-portfolio.png&w=384&q=75",
-      width: 1159,
-      height: 820,
-    },
-  },
+  title: "John Bars Quipia",
+  description:
+    "Hi! I'm John Bars, a frontend developer specializing in React and Next.js.",
+  image:
+    "https://john-bars.vercel.app/_next/image?url=%2Fimages%2Fportfolio-mobile.png&w=384&q=75",
 };
 
 export default function RootLayout({ children }) {
