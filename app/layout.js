56aa46d951200d094205d513caf838f305fc6480
John Bars <john.barsq@gmail.com>
2024-02-16 19:22:40 +0800

    add the url of the image

--- app/layout.js
+++ app/layout.js
@@ -8,8 +8,8 @@ export const metadata = {
     title: "John Bars Quipia",
     description:
       "Hi! I'm John Bars, a frontend developer specializing in React and Next.js.",
-    Image: {
-      url: "",
+    Images: {
+      url: "https://john-bars.vercel.app/_next/image?url=%2Fimages%2Fjohn-bars-portfolio.png&w=384&q=75",
       width: 1159,
       height: 820,
     },
