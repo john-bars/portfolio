f13e48532aca1787ce2c2da13a68617c6d06aa60
John Bars <john.barsq@gmail.com>
2024-02-18 12:21:26 +0800

    add my github and email in the Intro for mobile devices

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { socials } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Intro() {
   const { ref } = useSectionInView("Intro");
@@ -11,6 +14,26 @@ export default function Intro() {
       <p className="mt-4 opacity-70 max-md:self-center md:text-3xl">
         React Front End Developer
       </p>
+      <ul className="mt-3 flex justify-center gap-5 md:hidden">
+        {socials.map((social) => (
+          <li key={social.name}>
+            <Link
+              href={social.href}
+              target="_blank"
+              rel="noreferrer noopener"
+              title={social.name}
+            >
+              <Image
+                src={social.imgSrc}
+                alt={`${social.name} icon`}
+                width={20}
+                height={20}
+                className="opacity-40 invert hover:opacity-100"
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
